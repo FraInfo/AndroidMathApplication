4838aad96cae944cfa61453915a1482a288ea66b
FraInfo <74244750+FraInfo@users.noreply.github.com>
2021-11-21 21:02:11 +0100

    Feature: implementation of Newton-Raphson method.

---
+++ app/src/main/java/it/edu/ChartApplication/NewtonRaphsonActivity.java
@@ -0,0 +1,91 @@
+package it.edu.ChartApplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.CombinedChart;
+import com.github.mikephil.charting.data.CombinedData;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import it.edu.ChartApplication.Functions.Bisection;
+import it.edu.ChartApplication.Functions.NewtonRaphson;
+
+public class NewtonRaphsonActivity extends AppCompatActivity {
+
+    private CombinedChart combinedChart;
+    private EditText editTextFunctionA;
+    private Switch switchAnimate;
+    public TextView textViewBisection;
+    private EditText _editTextA;
+    private EditText _editTextB;
+    private EditText _editTextA2;
+    private EditText _editTextB2;
+
+    boolean animate = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newton_raphson);
+
+        combinedChart = findViewById(R.id.combined_chart);
+        editTextFunctionA = findViewById(R.id.editTextFunctionA);
+        textViewBisection = findViewById(R.id.textViewBisection);
+        _editTextA = findViewById(R.id.editTextIntervalA);
+        _editTextB = findViewById(R.id.editTextIntervalB);
+        _editTextA2 = findViewById(R.id.editTextIntervalA2);
+        _editTextB2 = findViewById(R.id.editTextIntervalB2);
+        switchAnimate = findViewById(R.id.switchAnimate);
+
+    }
+
+    public void sendValueCalc(View view) {
+        drawFunction();
+        combinedChart.setVisibility(View.VISIBLE);
+    }
+
+    public void drawFunction() {
+        try {
+            String function = "x=0;" + editTextFunctionA.getText();
+
+            NewtonRaphson newtonRaphson = new NewtonRaphson(function);
+
+            float _a = Float.parseFloat(String.valueOf(_editTextA.getText()));
+            float _b = Float.parseFloat(String.valueOf(_editTextB.getText()));
+            float a = Float.parseFloat(String.valueOf(_editTextA2.getText()));
+            float b = Float.parseFloat(String.valueOf(_editTextB2.getText()));
+
+            newtonRaphson.drawFunction(_a, _b, function);
+            textViewBisection.setText("Zero of function: " + Float.parseFloat(String.valueOf(Double.valueOf(newtonRaphson.getMethod(a,b)))));
+
+            LineDataSet lineDataSet = new LineDataSet(newtonRaphson.getLineEntries(), "Function");
+            LineData lineData = new LineData (lineDataSet);
+            lineDataSet.setDrawCircles(false);
+            lineDataSet.setLineWidth(2f);
+            CombinedData combinedData = new CombinedData();
+            combinedData.setData(lineData);
+            combinedChart.getDescription().setText("Bisection");
+            combinedChart.setData(combinedData);
+            combinedChart.invalidate();
+
+            animate = switchAnimate.isChecked() ? true : false;
+
+            if (animate){
+                combinedChart.animateX(5000);
+                combinedChart.animateY(5000);
+            }
+
+        } catch (Exception e) {
+            Log.e("SubtendedAreaActivity","Exception trying to set up chart");
+            e.printStackTrace();
+        }
+    }
+
+}
