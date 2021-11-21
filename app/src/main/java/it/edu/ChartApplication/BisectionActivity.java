8639bd106f0ca3ff05bccd6fd14cab58068fb951
FraInfo <74244750+FraInfo@users.noreply.github.com>
2021-11-21 19:02:11 +0100

    Fix: compute value of function's zero.

--- app/src/main/java/it/edu/ChartApplication/BisectionActivity.java
+++ app/src/main/java/it/edu/ChartApplication/BisectionActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.CombinedChart;
 import com.github.mikephil.charting.data.CombinedData;
@@ -20,10 +21,11 @@ public class BisectionActivity extends AppCompatActivity {
     private CombinedChart combinedChart;
     private EditText editTextFunctionA;
     private Switch switchAnimate;
-    private Switch switchABS;
-    private EditText editTextPoint;
+    public TextView textViewBisection;
     private EditText _editTextA;
     private EditText _editTextB;
+    private EditText _editTextA2;
+    private EditText _editTextB2;
 
     boolean animate = false;
 
@@ -34,9 +36,11 @@ public class BisectionActivity extends AppCompatActivity {
 
         combinedChart = findViewById(R.id.combined_chart);
         editTextFunctionA = findViewById(R.id.editTextFunctionA);
-        editTextPoint = findViewById(R.id.editTextNumberPoints);
+        textViewBisection = findViewById(R.id.textViewBisection);
         _editTextA = findViewById(R.id.editTextIntervalA);
         _editTextB = findViewById(R.id.editTextIntervalB);
+        _editTextA2 = findViewById(R.id.editTextIntervalA2);
+        _editTextB2 = findViewById(R.id.editTextIntervalB2);
         switchAnimate = findViewById(R.id.switchAnimate);
     }
 
@@ -55,9 +59,11 @@ public class BisectionActivity extends AppCompatActivity {
 
             float _a = Float.parseFloat(String.valueOf(_editTextA.getText()));
             float _b = Float.parseFloat(String.valueOf(_editTextB.getText()));
+            float a = Float.parseFloat(String.valueOf(_editTextA2.getText()));
+            float b = Float.parseFloat(String.valueOf(_editTextB2.getText()));
 
-
-            bisection.getChart(_a, _b, function);
+            bisection.drawFunction(_a, _b, function);
+            textViewBisection.setText("Zero of function: " + Float.parseFloat(String.valueOf(Double.valueOf(bisection.getBisection(a,b)))));
 
             LineDataSet lineDataSet = new LineDataSet(bisection.getLineEntries(), "Function");
             LineData lineData = new LineData (lineDataSet);
